ae35321284e1c949df37711243e8b9485a3b0b37
baix@shumei.ai <baix@shumei.ai>
2020-12-08 09:26:55 +0800

    base

--- src/func.ts
+++ src/func.ts
@@ -21,3 +21,43 @@ let myAdd1: (x: number, y: number) => number =
 let myAdd2: (baseValue: number, increment: number) => number = function(x, y) { return x + y }
 
 // 可选参数
+function buildName(firstName: string, lastName?: string) {
+  if (lastName) {
+    return firstName + '' + lastName
+  } else {
+    return firstName
+  }
+}
+
+buildName('Bob', 'Adams')
+
+// 剩余参数
+function buildName1(firstName: string, ...restOfName: string[]) {
+  return firstName + '' + restOfName.join(',')
+}
+
+let buildNameFun: (fname: string, ...rest: string[]) => string = buildName1
+
+// this
+let deck = {
+  suits: ['heart', 'spades', 'clubs', 'diamonds'],
+  cards: Array(52),
+  createCardPicker: function() {
+    return () => {
+      let pickedCard = Math.floor(Math.random() * 52)
+      let pickedSuit = Math.floor(pickedCard / 13)
+
+      return {suit: this.suits[pickedSuit], card: pickedCard % 13}
+    }
+  }
+}
+
+let cardPicker = deck.createCardPicker()
+let pickedCard = cardPicker()
+
+console.log('card: ' + pickedCard.card + ' of ' + pickedCard.suit)
+
+// this 参数
+function f(this: void) {
+  // ...
+}
